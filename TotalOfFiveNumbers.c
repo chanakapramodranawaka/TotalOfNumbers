ebbdb477b5070b8f663e544470c77ede7add65a8
Chanaka Pramod <67319115+chanakapramodranawaka@users.noreply.github.com>
2023-10-02 09:52:04 +0530

    V1

---
+++ TotalOfFiveNumbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(void)
+{
+    int count = 0,total;
+
+    if(count>=5)
+    {
+        total = total+count;
+        count = count +1;
+    }
+
+    else
+    {
+        printf("Total is %d",total);
+    }
+
+    return 0;
+}
